1b16fdc782895850a38f388ced59edbe65923bff
khardrix <carmenandryanhuffman@yahoo.com>
2020-05-13 19:20:21 -0400

    fourth commit

--- src/App.js
+++ src/App.js
@@ -25,11 +25,10 @@ class App extends Component {
         return (
             <div className="App">
             <CardList name = 'Yihua'>
-                <h1>Yihua</h1>
-            </CardList>
                 {
                     this.state.monsters.map(monster => <h1>{ monster.name }</h1>)
                 }
+            </CardList>
             </div>
         );
     }
