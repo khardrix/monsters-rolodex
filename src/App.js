f4947fd7381ce8b6b388e6a32cd27cacce9ae719
khardrix <carmenandryanhuffman@yahoo.com>
2020-05-15 23:13:25 -0400

    seventh commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,12 @@ class App extends Component {
 
     render(){
         return (
-            <div className="App">
+            <div className='App'>
+            <input 
+                type='search' 
+                placeholder='search monsters' 
+                onChange={e => this.setState({ searchField: e.target.value })}   
+            />>
             <CardList monsters = { this.state.monsters } />
             </div>
         );
