59f013dfee8ce64c04ca08e0d3c5bcb838f2f08e
khardrix <carmenandryanhuffman@yahoo.com>
2020-05-16 22:44:27 -0400

    eighth commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
         super();
 
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ''
         };
     }
 
@@ -22,6 +23,12 @@ class App extends Component {
     }
 
     render(){
+
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+
         return (
             <div className='App'>
                 <input 
@@ -29,7 +36,7 @@ class App extends Component {
                     placeholder='search monsters' 
                     onChange={e => this.setState({ searchField: e.target.value })}   
                 />
-                <CardList monsters = { this.state.monsters } />
+                <CardList monsters = { filteredMonsters } />
             </div>
         );
     }
