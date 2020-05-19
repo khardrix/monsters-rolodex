1a0aebc6fec7240d8374218bc1298af83a6541dc
khardrix <carmenandryanhuffman@yahoo.com>
2020-05-19 01:08:11 -0400

    eleventh commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
             searchField: ''
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount(){
@@ -25,7 +25,8 @@ class App extends Component {
         .then(users => this.setState({ monsters: users }));
     }
 
-    handleChange(e){
+    // handleChange(e){
+    handleChange = e => {
         this.setState({ searchField: e.target.value })
     }
 
