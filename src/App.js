5531b54968432cf8d5399f410aef4dd9b13dfc01
khardrix <carmenandryanhuffman@yahoo.com>
2020-05-20 00:08:09 -0400

    eleventh commit / very minor update

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ class App extends Component {
     }
 
     render(){
-
         const { monsters, searchField } = this.state;
         const filteredMonsters = monsters.filter(monster =>
             monster.name.toLowerCase().includes(searchField.toLowerCase())
